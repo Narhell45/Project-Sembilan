185125e110baedd5b0e9d2b7ecba051431e103b4
florencia32 <florenciafiocenzafelixyulianto@gmail.com>
2025-10-21 09:08:38 +0700

    database, bro

---
+++ database/gudang.sql
@@ -0,0 +1,87 @@
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: gudang
+-- ------------------------------------------------------
+-- Server version	10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `inventaris`
+--
+
+DROP TABLE IF EXISTS `inventaris`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `inventaris` (
+  `id_barang` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_barang` varchar(20) NOT NULL,
+  `kode_barang` varchar(70) NOT NULL,
+  `jumlah` int(45) NOT NULL,
+  `kondisi` enum('baik','rusak','hilang') NOT NULL,
+  `lokasi` varchar(32) NOT NULL,
+  `tanggal_input` date NOT NULL,
+  `id_user` int(67) DEFAULT NULL,
+  PRIMARY KEY (`id_barang`),
+  KEY `fk_inventaris_user` (`id_user`),
+  CONSTRAINT `fk_inventaris_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventaris`
+--
+
+LOCK TABLES `inventaris` WRITE;
+/*!40000 ALTER TABLE `inventaris` DISABLE KEYS */;
+INSERT INTO `inventaris` VALUES (13,'AK-47','SNJ-907-774',50,'baik','Gudang senjata 1','2025-10-13',3),(14,'Thompson','SNJ-18181-1991',42,'baik','Gudang senjata 2','2025-10-13',2),(15,'Revolver','SNJ-990-678',5,'rusak','Gudang senjata 3','2025-10-13',2),(16,'FAMAS','SNJ-445-908',7,'hilang','Gudang senjata 4','2025-10-13',3),(17,'M16','SNJ-556-897',65,'baik','Gudang senjata 5','2025-10-13',2),(18,'M4A1','SNJ-888-873',90,'baik','Gudang senjata 6','2025-10-13',3);
+/*!40000 ALTER TABLE `inventaris` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(80) NOT NULL,
+  `nama_lengkap` varchar(30) NOT NULL,
+  `level` enum('admin','petugas') NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'admin1','14451','Bagaskarawijaya','admin','2025-10-13 18:18:17'),(2,'petugas1','44256','Baskoro','petugas','2025-10-13 18:18:17'),(3,'petugas2','67445','Bimasakti Wiratama','petugas','2025-10-13 18:18:17');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-10-21  9:03:50
